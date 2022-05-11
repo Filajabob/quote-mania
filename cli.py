d39b3d61edb9f9462715b2e168f358b91e7ede3f
Liam <liam.yy.xu@gmail.com>
2022-05-11 11:48:21 -0400

    Quotes added to assets/game/quotes.json (automatic process)

--- cli.py
+++ cli.py
@@ -1,9 +1,26 @@
 import json
+import subprocess
 
+
+print("Welcome to the Quote Mania Quote Adding Tool! "
+      "To stop add quotes and save your changes, press Ctrl+C. "
+      "Please note that Git must be installed and you need to have a Github account that has access to Filajabob/quote-mania")
 
 while True:
-    student = input("Student: ").capitalize()
-    quote = input("Quote: ")
+    try:
+        student = input("Student: ").capitalize()
+        quote = input("Quote: ")
+    except KeyboardInterrupt:
+        print("Staging changes...")
+        subprocess.run("git add .")
+        print("Changes staged.")
+        subprocess.run("git commit -m \"Quotes added to assets/game/quotes.json (automatic process)\"")
+        print("Changes commited.")
+        subprocess.run("git push https://github.com/Filajabob/quote-mania master")
+        print("Changes pushed to https://github.com/Filajabob/quote-mania")
+
+        print("Quitting...")
+        break
 
     with open("assets/game/quotes.json", 'r+') as f:
         data = json.load(f)
